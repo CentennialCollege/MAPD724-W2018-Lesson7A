7aa8ee6733004f3045e19a2c859d33de5834065e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-03-05 22:11:55 -0500

    added Android build

---
+++ Assets/_Scripts/ClickMeButtonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickMeButtonBehaviour : MonoBehaviour {
+
+	// Public Instance Variables
+	public Button ClickMeButton;
+	public Text HelloWorldLabel;
+
+	// Use this for initialization
+	void Start () {
+		this.ClickMeButton.onClick.AddListener (this.ClickHandler);
+	}
+
+	// Click event handler
+	public void ClickHandler() {
+		this.HelloWorldLabel.text = "Clicked!";
+	}
+
+}
